a49c8ca8468aa66df80e8dd7993336f6ac859880
Afiqrmadhan <muhammadafiqramadhan@students.undip.ac.id>
2025-03-01 22:48:24 +0700

    fix banget

--- app/Models/ProjectModel.php
+++ app/Models/ProjectModel.php
@@ -8,7 +8,7 @@ class ProjectModel extends Model
 {
     protected $table = 'project';
     protected $primaryKey = 'Id';
-    protected $allowedFields = ['ProjectManager', 'ProjectManagerId', 'Title', 'ClientName', 'ProjectSchedule', 'UATHistoryId'];
+    protected $allowedFields = ['ProjectManager', 'ProjectManagerId', 'Title', 'ClientCompany', 'ClientName', 'ProjectSchedule', 'UATHistoryId'];
 
     // Fungsi untuk memanggil procedure AddProject
     public function addProjectUsingProcedure($ProjectManager, $Title, $ClientCompany, $ClientName, $ProjectSchedule)
